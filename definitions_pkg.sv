94cb689a674bf2665781c7211ffdf95138683d24
Aayush Jha <jhaaayush.np@gmail.com>
2025-03-22 00:29:16 -0400

    UART transmitter receiver and fifo implemented and verified

--- definitions_pkg.sv
+++ definitions_pkg.sv
@@ -2,16 +2,16 @@ package definitions_pkg;
   parameter IMAGE_WIDTH   = 512;
 
   // FIFO params
-
   parameter FIFO_WIDTH = 8;         // each entry is 1 byte pixel
-  // parameter FIFO_DEPTH = 8192;   // can buffer upto 16 image rows
-  parameter FIFO_DEPTH = 8;         // fifo vector test size for test bench  
+  parameter FIFO_DEPTH = 8192;   // can buffer upto 16 image rows
 
   // UART params
   parameter int   CLK_FREQ      = 10_000_000;              
-  parameter int   BAUD_RATE     = 625000;                     
+  parameter int   BAUD_RATE     = 312500;                     
   parameter real  CLK_PER_BIT   = CLK_FREQ / BAUD_RATE;        
-  parameter int   OVERSAMPLE    = 16;                           
+  parameter int   OVERSAMPLE    = 16;      
+  parameter int   HOLD_TIME     = OVERSAMPLE / 2;
+  parameter int   DIVISOR       = CLK_PER_BIT / OVERSAMPLE;                     
 
   const logic [7:0] gaussian_kernel_3 [0:8] =  '{
     8'h1, 8'h2, 8'h1, 
